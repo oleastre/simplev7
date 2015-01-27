1812088ab35aa0e4b1ba8843f3856b4486ef69df
Olivier Samyn <code@oleastre.be>
2015-01-27 22:37:46 +0100

    \t => ..

--- decorations.lua
+++ decorations.lua
@@ -3,212 +3,212 @@
 -- Flowers
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 436,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:rose",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 436,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:rose",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 19822,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:tulip",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 19822,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:tulip",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 1220999,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:dandelion_yellow",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 1220999,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:dandelion_yellow",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 36662,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:geranium",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 36662,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:geranium",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 1133,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:viola",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 1133,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:viola",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.015,
-		scale = 0.03,
-		spread = {x=100, y=100, z=100},
-		seed = 73133,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "flowers:dandelion_white",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.015,
+    scale = 0.03,
+    spread = {x=100, y=100, z=100},
+    seed = 73133,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "flowers:dandelion_white",
 })
 
 -- Grasses
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0.04,
-		scale = 0.08,
-		spread = {x=100, y=100, z=100},
-		seed = 66440,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "default:grass_1",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = 0.04,
+    scale = 0.08,
+    spread = {x=100, y=100, z=100},
+    seed = 66440,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "default:grass_1",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0.02,
-		scale = 0.08,
-		spread = {x=100, y=100, z=100},
-		seed = 66440,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "default:grass_2",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = 0.02,
+    scale = 0.08,
+    spread = {x=100, y=100, z=100},
+    seed = 66440,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "default:grass_2",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0,
-		scale = 0.08,
-		spread = {x=100, y=100, z=100},
-		seed = 66440,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "default:grass_3",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = 0,
+    scale = 0.08,
+    spread = {x=100, y=100, z=100},
+    seed = 66440,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "default:grass_3",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.02,
-		scale = 0.08,
-		spread = {x=100, y=100, z=100},
-		seed = 66440,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "default:grass_4",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.02,
+    scale = 0.08,
+    spread = {x=100, y=100, z=100},
+    seed = 66440,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "default:grass_4",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.04,
-		scale = 0.08,
-		spread = {x=100, y=100, z=100},
-		seed = 66440,
-		octaves = 3,
-		persist = 0.6
-	},
-	biomes = {"prairie", "meadow"},
-	y_min = -32000,
-	y_max = 32000,
-	decoration = "default:grass_5",
+  deco_type = "simple",
+  place_on = {"default:dirt_with_grass"},
+  sidelen = 16,
+  noise_params = {
+    offset = -0.04,
+    scale = 0.08,
+    spread = {x=100, y=100, z=100},
+    seed = 66440,
+    octaves = 3,
+    persist = 0.6
+  },
+  biomes = {"prairie", "meadow"},
+  y_min = -32000,
+  y_max = 32000,
+  decoration = "default:grass_5",
 })
 
 minetest.register_decoration({
-	deco_type = "simple",
-	place_on = "default:dirt_with_grass",
-	sidelen = 16,
-	fill_ratio = 0.17,
-	biomes = {"jungle"},
-	decoration = {"default:junglegrass"},
+  deco_type = "simple",
+  place_on = "default:dirt_with_grass",
+  sidelen = 16,
+  fill_ratio = 0.17,
+  biomes = {"jungle"},
+  decoration = {"default:junglegrass"},
 })
 
 -- Saplings
--- mapgen.lua
+++ mapgen.lua
@@ -1,158 +1,198 @@
 minetest.clear_registered_biomes()
 
 minetest.register_biome({
-	name = "forest",
-	node_top = "default:dirt_with_grass",		depth_top = 1,
-	node_filler = "default:dirt",			depth_filler = 3,
-	y_min = 2,					y_max =40,
-	heat_point = 50,				humidity_point = 80,
+  name = "forest",
+  node_top = "default:dirt_with_grass",
+  depth_top = 1,
+  node_filler = "default:dirt",
+  depth_filler = 3,
+  y_min = 2,
+  y_max =40,
+  heat_point = 50,
+  humidity_point = 80,
 })
 
 minetest.register_biome({
-	name = "meadow",
-	node_top = "default:dirt_with_grass",		depth_top = 1,
-	node_filler = "default:dirt",			depth_filler = 3,
-	y_min = 2,					y_max = 40,
-	heat_point = 50,				humidity_point = 30,
+  name = "meadow",
+  node_top = "default:dirt_with_grass",
+  depth_top = 1,
+  node_filler = "default:dirt",
+  depth_filler = 3,
+  y_min = 2,
+  y_max = 40,
+  heat_point = 50,
+  humidity_point = 30,
 })
 
 minetest.register_biome({
-	name = "beach",
-	node_top = "default:sand",			depth_top = 2,
-	y_min = -500,				y_max = 2,
-	heat_point = 40,				humidity_point = 50,
+  name = "beach",
+  node_top = "default:sand",
+  depth_top = 2,
+  y_min = -500,
+  y_max = 2,
+  heat_point = 40,
+  humidity_point = 50,
 })
 
 minetest.register_biome({
-	name = "tropical_beach",
-	node_top = "default:sand",			depth_top = 5,
-	y_min = -500, 				y_max = 3,
-	heat_point = 80.0,				humidity_point = 50,
+  name = "tropical_beach",
+  node_top = "default:sand",
+  depth_top = 5,
+  y_min = -500,
+  y_max = 3,
+  heat_point = 80.0,
+  humidity_point = 50,
 })
 
 minetest.register_biome({
-	name = "jungle",
-	node_top = "default:dirt_with_grass",		depth_top = 1,
-	node_filler = "default:dirt",			depth_filler = 3,
-	y_min = 2,					y_max = 40,
-	heat_point = 80,				humidity_point = 80,
+  name = "jungle",
+  node_top = "default:dirt_with_grass",
+  depth_top = 1,
+  node_filler = "default:dirt",
+  depth_filler = 3,
+  y_min = 2,
+  y_max = 40,
+  heat_point = 80,
+  humidity_point = 80,
 })
 
 minetest.register_biome({
-	name = "desert",
-	node_top = "default:desert_sand",		depth_top = 3,
-	node_filler = "default:desert_stone",		depth_filler = 57,
-	y_min = 2,					y_max = 40,
-	heat_point = 80,				humidity_point = 20,
+  name = "desert",
+  node_top = "default:desert_sand",
+  depth_top = 3,
+  node_filler = "default:desert_stone",
+  depth_filler = 57,
+  y_min = 2,
+  y_max = 40,
+  heat_point = 80,
+  humidity_point = 20,
 })
 
 minetest.register_biome({
-	name = "mountain",
-	node_top = "default:snowblock",			depth_top = 1,
-	node_filler = "default:dirt_with_snow",		depth_filler = 1,
-	y_min = 41,				y_max = 80,
+  name = "mountain",
+  node_top = "default:snowblock",
+  depth_top = 1,
+  node_filler = "default:dirt_with_snow",
+  depth_filler = 1,
+  y_min = 41,
+  y_max = 80,
 })
 
 minetest.register_biome({
-	name = "alpine",
-	node_top = "default:snowblock",			depth_top = 1,
-	node_filler = "default:dirt_with_snow",		depth_filler = 1,
-	y_min = 81,				y_max = 300,
+  name = "alpine",
+  node_top = "default:snowblock",
+  depth_top = 1,
+  node_filler = "default:dirt_with_snow",
+  depth_filler = 1,
+  y_min = 81,
+  y_max = 300,
 })
 
 minetest.register_biome({
-	name = "dune",
-	node_top = "default:sand",			depth_top = 3,
-	node_filler = "default:dirt",			depth_filler = 2,
-	y_min = 1,					y_max = 30,
-	heat_point = 10,				humidity_point = 10,
+  name = "dune",
+  node_top = "default:sand",
+  depth_top = 3,
+  node_filler = "default:dirt",
+  depth_filler = 2,
+  y_min = 1,
+  y_max = 30,
+  heat_point = 10,
+  humidity_point = 10,
 })
 
 minetest.register_biome({
-	name = "prairie",
-	node_top = "default:dirt_with_grass",		depth_top = 1,
-	node_filler = "default:dirt",			depth_filler = 4,
-	y_min = 10,				y_max = 40,
-	heat_point = 40,				humidity_point = 20,
+  name = "prairie",
+  node_top = "default:dirt_with_grass",
+  depth_top = 1,
+  node_filler = "default:dirt",
+  depth_filler = 4,
+  y_min = 10,
+  y_max = 40,
+  heat_point = 40,
+  humidity_point = 20,
 })
 
 minetest.register_biome({
-	name = "papyrus",
-	node_top = "default:dirt_with_grass",		depth_top = 1,
-	node_filler = "default:dirt",			depth_filler = 2,
-	y_min = 1,					y_max = 1,
-	heat_point = 60,				humidity_point = 50,
+  name = "papyrus",
+  node_top = "default:dirt_with_grass",
+  depth_top = 1,
+  node_filler = "default:dirt",
+  depth_filler = 2,
+  y_min = 1,
+  y_max = 1,
+  heat_point = 60,
+  humidity_point = 50,
 })
 
 
 
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:dirt_with_grass",
---	sidelen = 16,
---	fill_ratio = 0.037,
---	biomes = {"forest"},
---	flags = "place_center_x, place_center_z",
---	schematic = minetest.get_modpath("simplev7").."/schematics/appletree.mts",
+--  deco_type = "schematic",
+--  place_on = "default:dirt_with_grass",
+--  sidelen = 16,
+--  fill_ratio = 0.037,
+--  biomes = {"forest"},
+--  flags = "place_center_x, place_center_z",
+--  schematic = minetest.get_modpath("simplev7").."/schematics/appletree.mts",
 --})
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = {"default:dirt_with_grass", "default:dirt"},
---	sidelen = 80,
---	fill_ratio = 0.003,
---	biomes = {"meadow"},
---	flags = "place_center_x, place_center_z",
---	schematic = minetest.get_modpath("simplev7").."/schematics/appletree.mts",
+--  deco_type = "schematic",
+--  place_on = {"default:dirt_with_grass", "default:dirt"},
+--  sidelen = 80,
+--  fill_ratio = 0.003,
+--  biomes = {"meadow"},
+--  flags = "place_center_x, place_center_z",
+--  schematic = minetest.get_modpath("simplev7").."/schematics/appletree.mts",
 --})
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:dirt_with_grass",
---	sidelen = 16,
---	fill_ratio = 0.057,
---	biomes = {"jungle"},
---	flags = "place_center_x, place_center_z",
---	schematic = minetest.get_modpath("simplev7").."/schematics/jungletree.mts",
+--  deco_type = "schematic",
+--  place_on = "default:dirt_with_grass",
+--  sidelen = 16,
+--  fill_ratio = 0.057,
+--  biomes = {"jungle"},
+--  flags = "place_center_x, place_center_z",
+--  schematic = minetest.get_modpath("simplev7").."/schematics/jungletree.mts",
 --})
 
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:snowblock",
---	sidelen = 16,
---	fill_ratio = 0.047,
---	flags = "place_center_x, place_center_z",
---	biomes = {"mountain"},
---	schematic = minetest.get_modpath("simplev7").."/schematics/snowtree.mts",
+--  deco_type = "schematic",
+--  place_on = "default:snowblock",
+--  sidelen = 16,
+--  fill_ratio = 0.047,
+--  flags = "place_center_x, place_center_z",
+--  biomes = {"mountain"},
+--  schematic = minetest.get_modpath("simplev7").."/schematics/snowtree.mts",
 --})
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:dirt_with_grass",
---	sidelen = 16,
---	fill_ratio = 0.17,
---	biomes = {"papyrus"},
---	schematic = minetest.get_modpath("simplev7").."/schematics/papyrus.mts",
+--  deco_type = "schematic",
+--  place_on = "default:dirt_with_grass",
+--  sidelen = 16,
+--  fill_ratio = 0.17,
+--  biomes = {"papyrus"},
+--  schematic = minetest.get_modpath("simplev7").."/schematics/papyrus.mts",
 --})
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:desert_sand",
---	sidelen = 64,
---	noise_params = {offset = -0.005, scale = 0.03, spread = {x = 320, y = 320, z = 320}, seed = 230, octaves = 3, persist = 0.6},
---	biomes = {"desert"},
---	schematic = minetest.get_modpath("simplev7").."/schematics/cactus.mts",
+--  deco_type = "schematic",
+--  place_on = "default:desert_sand",
+--  sidelen = 64,
+--  noise_params = {offset = -0.005, scale = 0.03, spread = {x = 320, y = 320, z = 320}, seed = 230, octaves = 3, persist = 0.6},
+--  biomes = {"desert"},
+--  schematic = minetest.get_modpath("simplev7").."/schematics/cactus.mts",
 --})
 
 --minetest.register_decoration({
---	deco_type = "schematic",
---	place_on = "default:dirt_with_grass",
---	sidelen = 16,
---	fill_ratio = 0.17,
---	biomes = {"papyrus"},
---	schematic = minetest.get_modpath("simplev7").."/schematics/papyrus.mts",
+--  deco_type = "schematic",
+--  place_on = "default:dirt_with_grass",
+--  sidelen = 16,
+--  fill_ratio = 0.17,
+--  biomes = {"papyrus"},
+--  schematic = minetest.get_modpath("simplev7").."/schematics/papyrus.mts",
 --})
 
 
